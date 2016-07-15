832ad9f39aed63995b50603e78c6870439968db6
zhanghanlin <andyzhanghanlin@163.com>
2016-07-15 16:17:09 +0800

    增加动态定时任务

--- src/sql/task.sql
+++ src/sql/task.sql
@@ -1,10 +1,10 @@
-CREATE TABLE job
+CREATE TABLE task
 (
     id VARCHAR(255) PRIMARY KEY NOT NULL,
     name VARCHAR(255),
-    job_group VARCHAR(255),
+    task_group VARCHAR(255),
     status VARCHAR(255),
-    cronExpression VARCHAR(255),
+    cron_expression VARCHAR(255),
     description VARCHAR(255),
     bean_class VARCHAR(255),
     method_param VARCHAR(255),
